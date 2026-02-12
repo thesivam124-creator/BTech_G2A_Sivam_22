0e644a793bf13344544bfb1caba5cdff7f9d214b
thesivam124-creator <thesivam124@gmail.com>
2026-02-12 16:00:52 +0530

    Add files via upload

---
+++ same or not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("enter the two numbers");
+    scanf("%d%d",&a,&b);
+    if(a==b)
+    {printf("the numbers entered by the user is same");}
+    else
+    {
+        printf("the numbers entered by the user are not same");
+    }
+
+}
+    
+    
