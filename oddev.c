b2b288c1ea79569eebe6ea95f405760a4d2f24de
thesivam124-creator <thesivam124@gmail.com>
2026-02-16 23:02:01 +0530

    Add files via upload

---
+++ oddev.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a;
+    printf("enter the number:");
+    scanf("%d",&a);
+    if(a%2==0) {
+        printf("the entered number is even");}
+    else 
+        { printf("the entered number is odd");
+    }
+    }
