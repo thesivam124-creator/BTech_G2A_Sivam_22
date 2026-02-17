5ebf4a14c72d58bfdae015af483aec3a314354e3
thesivam124-creator <thesivam124@gmail.com>
2026-02-17 22:39:54 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;  
+    }
+
+    printf("The sum of numbers from 1 to %d is %d\n", n, sum);
+    return 0;
+}
