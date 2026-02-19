72e0fd162376ef13168bf82051053450cf251515
thesivam124-creator <thesivam124@gmail.com>
2026-02-19 14:24:03 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main() {
+    printf("enter the letter:");
+    char a;
+    scanf("%c",&a);
+    switch (a)
+    {
+    case 'a':
+    printf("the entered letter is vowel");
+    break;
+    case 'e':
+    printf("the entered letter is vowel");
+    break;
+    case 'i':
+    printf("the entered letter is vowel");
+    break;
+    case 'o':
+    printf("the entered letter is vowel");
+    break;
+    printf("the entered letter is vowel");
+    break;
+    case 'A':
+    printf("the entered letter is vowel");
+    break;
+    case 'E':
+    printf("the entered letter is vowel");
+    break;
+    case 'I':
+    printf("the entered letter is vowel");
+    break;
+    case 'O':
+    printf("the entered letter is vowel");
+    break;
+    case 'U':
+    printf("the entered letter is vowel");
+    break;
+    
+    
+    
+    default:
+    printf("the entered letter is consonant");
+        break;
+    }
+}
