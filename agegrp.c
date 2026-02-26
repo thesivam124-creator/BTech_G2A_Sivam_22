5e5c5ad475061207f5a81e34162978001f12725d
thesivam124-creator <thesivam124@gmail.com>
2026-02-26 14:49:46 +0530

    Add files via upload

---
+++ agegrp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main( {
+    printf(""ENTER YOUR AGE GROUP:);
+    int a;
+    scanf("%d",a);
+if(a>=60) {
+    printf("SENIOR CITIZEN")
+}
+else if(a<60 && a>18) {
+    printf("ÄDULT")
+}
+else if(a>12 && a<=18) {
+    printf("TEENAGER")
+}
+else {
+    printf("CHILD")
+}
+}
